bba2a59fcffb4347c33edd3f81649e56cb5b831f
juheegg <eespacio95@gmail.com>
2020-10-22 23:59:36 +0900

    111PJT day09

---
+++ BOJ/b10/BOJ_10157_자리배정/Main.java
@@ -0,0 +1,27 @@
+package b10.BOJ_10157_자리배정;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int C = sc.nextInt();
+		int R = sc.nextInt();
+		int K = sc.nextInt();
+
+		int answer = 0;
+
+		if (C * R >= K) {
+			int x = 0, y = 0;
+			int[] len = { R - 1, C - 1, R - 1, C - 1 };
+			while(len[0] > 1)
+			for (int i = 0; i < 4; i++) {
+
+			}
+		}
+
+		System.out.println(answer);
+	}
+
+}
