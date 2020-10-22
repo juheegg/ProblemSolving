2dfaf3e5335d072a396f048551e8d5a0fffac643
juheegg <eespacio95@gmail.com>
2020-10-23 00:30:12 +0900

    111PJT - day09

--- BOJ/b10/BOJ_10157_자리배정/Main.java
+++ BOJ/b10/BOJ_10157_자리배정/Main.java
@@ -4,24 +4,43 @@ import java.util.Scanner;
 
 public class Main {
 
+	static int C, R, K;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int C = sc.nextInt();
-		int R = sc.nextInt();
-		int K = sc.nextInt();
+		C = sc.nextInt();
+		R = sc.nextInt();
+		K = sc.nextInt();
 
-		int answer = 0;
+		String answer = "0";
 
 		if (C * R >= K) {
-			int x = 0, y = 0;
-			int[] len = { R - 1, C - 1, R - 1, C - 1 };
-			while(len[0] > 1)
-			for (int i = 0; i < 4; i++) {
-
-			}
+			answer = calc();
 		}
 
 		System.out.println(answer);
+	}
+
+	private static String calc() {
+		int x = 1, y = 0;
+		int cnt = 0;
+
+		int[] len = { R, C - 1, R - 1, C - 2 };
+		int[][] d = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+
+		while (cnt < K) {
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < len[i]; j++) {
+					x += d[i][0];
+					y += d[i][1];
+					if (++cnt == K) {
+						return x + " " + y;
+					}
+				}
+				len[i] -= 2;
+			}
+		}
+		return "0";
 	}
 
 }
