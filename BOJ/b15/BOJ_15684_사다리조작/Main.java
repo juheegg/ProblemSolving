5321517ea316f7f7088c9187e59a0b747be11ac5
juheegg <eespacio95@gmail.com>
2021-04-18 00:28:45 +0900

    [210417] 111PJT - 백트래킹

--- BOJ/b15/BOJ_15684_사다리조작/Main.java
+++ BOJ/b15/BOJ_15684_사다리조작/Main.java
@@ -2,102 +2,90 @@ package b15.BOJ_15684_사다리조작;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, M, H;
+	static int N, M, H, answer;
 	static boolean ladder[][];
-	static int cnt[];
-	static boolean flag;
-	static List<Integer> oddlist;
+	static final int INF = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws Exception {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
 
-		ladder = new boolean[H + 1][N];
-		cnt = new int[N];
+		answer = INF;
+		ladder = new boolean[H + 1][N + 1];
 
-		int a, b;
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			ladder[a][b] = true;
-			cnt[b]++;
 		}
 
-		int odd = 0;
-		oddlist = new ArrayList<>();
-		for (int i = 1; i < N; i++) {
-			if (cnt[i] % 2 != 0) {
-				odd++;
-				oddlist.add(i);
-			}
-		}
+		if (getOddCnt() <= 3)
+			dfs(0);
 
-		if (N == 2) {
-			if (M % 2 == 0)
-				System.out.println(0);
-			else
-				System.out.println(1);
-		} else if (odd > 3) {
-			System.out.println(-1);
-		} else if (odd == 0) {
-			if (canReach()) {
-				System.out.println(0);
-			} else {
-				System.out.println(-1);
-			}
-		} else if (makeStep(0, odd)) {
-			System.out.println(odd);
-		} else {
-			System.out.println(-1);
-		}
-
+		System.out.println(answer == INF ? -1 : answer);
 	}
 
-	private static boolean makeStep(int n, int odd) {
-		if (n == odd) {
-			if (canReach())
-				flag = true;
-			return flag;
-		}
-		for (int i = 1; i <= H; i++) {
-			int j = oddlist.get(n);
-			if (ladder[i][j])
-				continue;
-			if ((j == 1 && !ladder[i][j + 1]) || (j == N - 1 && !ladder[i][j - 1])
-					|| (!ladder[i][j - 1] && !ladder[i][j + 1])) {
-				ladder[i][j] = true;
-				makeStep(n + 1, odd);
-				if (flag)
-					return true;
-				ladder[i][j] = false;
-			}
-
-		}
-		return false;
-	}
-
-	private static boolean canReach() {
+	private static int getOddCnt() {
+		int result = 0;
 		for (int c = 1; c < N; c++) {
-			int j = c;
-			for (int i = 1; i <= H; i++) {
-				if (j > 1 && ladder[i][j - 1]) {
-					j--;
-				} else if (j < N && ladder[i][j]) {
-					j++;
-				}
+			int cnt = 0;
+			for (int r = 1; r <= H; r++) {
+				if (ladder[r][c])
+					cnt++;
 			}
-			if (j != c)
+			if (cnt % 2 == 1)
+				result++;
+		}
+		return result;
+	}
+
+	private static void dfs(int depth) {
+		if (answer <= depth || depth > 3)
+			return;
+		if (check()) {
+			answer = Math.min(answer, depth);
+			return;
+		}
+
+		for (int r = 1; r <= H; r++) {
+			for (int c = 1; c < N; c++) {
+				if (!canPut(r, c) || ladder[r][c])
+					continue;
+				ladder[r][c] = true;
+				dfs(depth + 1);
+				ladder[r][c] = false;
+			}
+		}
+	}
+
+	private static boolean canPut(int r, int c) {
+		if (c != 1 && ladder[r][c - 1])
+			return false;
+		if (c != N && ladder[r][c + 1])
+			return false;
+		return true;
+	}
+
+	private static boolean check() {
+		int col = 0;
+		for (int c = 1; c <= N; c++) {
+			col = c;
+			for (int r = 1; r <= H; r++) {
+				if (col != 1 && ladder[r][col - 1])
+					col--;
+				else if (ladder[r][col])
+					col++;
+			}
+			if (col != c)
 				return false;
 		}
 		return true;
